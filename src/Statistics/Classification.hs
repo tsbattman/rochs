92f4c559ee8c3e01a90c6389a93ef78c0da3f290
Thayne Batty <tsbattman@gmail.com>
2018-12-27 10:39:09 +0800

    add Statistics.Classification module

---
+++ src/Statistics/Classification.hs
@@ -0,0 +1,8 @@
+
+module Statistics.Classification (
+    module Classification
+  ) where
+
+import Statistics.Classification.ConfusionMatrix as Classification
+import Statistics.Classification.ROC as Classification
+import Statistics.Classification.Types as Classification
