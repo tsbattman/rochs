46071d1f101368c171ff19a9c2ff499c856bf9e7
Thayne Batty <tsbattman@gmail.com>
2019-04-02 15:37:21 +0800

    unbox instance for ClassificationScore

--- src/Statistics/Classification/Types.hs
+++ src/Statistics/Classification/Types.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Statistics.Classification.Types (
     ClassificationResult(..)
@@ -11,10 +14,13 @@ module Statistics.Classification.Types (
 import Data.Ord (comparing)
 
 import Data.Csv ((.:), (.=), DefaultOrdered(..), ToNamedRecord(..), FromNamedRecord(..), namedRecord)
-import Data.Vector.Algorithms.Tim
+import Data.Vector.Algorithms.Tim (sortBy)
 import Test.QuickCheck (Arbitrary(..), choose)
 import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Generic.Mutable as GM
 import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as VU
 
 data ClassificationResult a = ClassificationResult {
     classifiedActual :: a
@@ -42,6 +48,36 @@ instance FromNamedRecord a => FromNamedRecord (ClassificationScore a) where
 
 instance Arbitrary a => Arbitrary (ClassificationScore a) where
   arbitrary = ClassificationScore <$> arbitrary <*> choose (0, 1)
+
+
+newtype instance VU.Unbox a => VU.MVector s (ClassificationScore a) = MV_ClassificationScore (VU.MVector s (a, Double))
+newtype instance VU.Unbox a => VU.Vector (ClassificationScore a) = V_ClassificationScore (VU.Vector (a, Double))
+
+instance VU.Unbox a => GM.MVector VU.MVector (ClassificationScore a) where
+  basicLength (MV_ClassificationScore v) = GM.basicLength v
+  basicUnsafeSlice i0 i1 (MV_ClassificationScore v) = MV_ClassificationScore $ GM.basicUnsafeSlice i0 i1 v
+  basicOverlaps (MV_ClassificationScore v0) (MV_ClassificationScore v1) = GM.basicOverlaps v0 v1
+  basicUnsafeNew n = MV_ClassificationScore <$> GM.basicUnsafeNew n
+  basicInitialize (MV_ClassificationScore v) = GM.basicInitialize v
+  basicUnsafeReplicate n (ClassificationScore a s) = MV_ClassificationScore <$> GM.basicUnsafeReplicate n (a, s)
+  basicUnsafeRead (MV_ClassificationScore v) i = uncurry ClassificationScore <$> GM.basicUnsafeRead v i
+  basicUnsafeWrite (MV_ClassificationScore v) i (ClassificationScore a s) = GM.basicUnsafeWrite v i (a, s)
+  basicClear (MV_ClassificationScore v) = GM.basicClear v
+  basicSet (MV_ClassificationScore v) (ClassificationScore a s) = GM.basicSet v (a, s)
+  basicUnsafeCopy (MV_ClassificationScore v0) (MV_ClassificationScore v1) = GM.basicUnsafeCopy v0 v1
+  basicUnsafeMove (MV_ClassificationScore v0) (MV_ClassificationScore v1) = GM.basicUnsafeMove v0 v1
+  basicUnsafeGrow (MV_ClassificationScore v) = fmap MV_ClassificationScore . GM.basicUnsafeGrow v
+
+instance VU.Unbox a => G.Vector VU.Vector (ClassificationScore a) where
+  basicUnsafeFreeze (MV_ClassificationScore mv) = V_ClassificationScore <$> G.basicUnsafeFreeze mv
+  basicUnsafeThaw (V_ClassificationScore v) = MV_ClassificationScore <$> G.basicUnsafeThaw v
+  basicLength (V_ClassificationScore v) = G.basicLength v
+  basicUnsafeSlice i0 i1 (V_ClassificationScore v) = V_ClassificationScore $ G.basicUnsafeSlice i0 i1 v
+  basicUnsafeIndexM (V_ClassificationScore v) i = uncurry ClassificationScore <$> G.basicUnsafeIndexM v i
+  basicUnsafeCopy (MV_ClassificationScore mv) (V_ClassificationScore v) = G.basicUnsafeCopy mv v
+  elemseq (V_ClassificationScore v) (ClassificationScore a s) = G.elemseq v (a, s)
+
+instance VU.Unbox a => VU.Unbox (ClassificationScore a)
 
 sortByScore :: V.Vector (ClassificationScore a) -> V.Vector (ClassificationScore a)
 sortByScore = V.modify (sortBy (comparing classifiedScore))
